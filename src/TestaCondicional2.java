9aa17bf328875e12bdc1fe97388c807ad426133d
Walquirialima <walquirialima2018@gmail.com>
2021-07-10 14:47:26 -0300

    java variaveis e fluxos

---
+++ src/TestaCondicional2.java
@@ -0,0 +1,20 @@
+
+public class TestaCondicional2 {
+
+	public static void main(String[] args) {
+		System.out.println("testando condicional");
+		int idade = 20;
+		int quantidadePessoas = 3;
+		boolean acompanhado = quantidadePessoas >= 3;
+
+		System.out.println("valor de acompanhado " + acompanhado);
+
+		if (idade >= 18 && acompanhado) {
+			System.out.println("Seja bem vindo");
+
+		} else {
+			System.out.println("Infelizmente você não pode entrar");
+		}
+	}
+
+}
---
+++ src/TestandoVariavel.java
@@ -0,0 +1,12 @@
+
+public class TestandoVariavel {
+	
+	public static void main(String[] args) {
+		int idade = 68;
+		boolean ehIdoso = idade >=65;
+		
+		System.out.println("idade do idoso "+ ehIdoso);
+		
+	}
+
+}
